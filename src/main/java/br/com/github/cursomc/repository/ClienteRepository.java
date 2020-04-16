b396bcfc9c89b9c3c3910de4140c8714cf0b142e
jcafure <cafure.jaime@gmail.com>
2020-04-15 22:06:42 -0400

    implementando autenticação

--- src/main/java/br/com/github/cursomc/repository/ClienteRepository.java
+++ src/main/java/br/com/github/cursomc/repository/ClienteRepository.java
@@ -3,9 +3,12 @@ package br.com.github.cursomc.repository;
 import br.com.github.cursomc.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Cliente findByEmail(String email);
 
 
 }
---
+++ src/main/java/br/com/github/cursomc/service/UserDetailService.java
@@ -0,0 +1,28 @@
+package br.com.github.cursomc.service;
+
+import br.com.github.cursomc.model.Cliente;
+import br.com.github.cursomc.repository.ClienteRepository;
+import br.com.github.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailService implements UserDetailsService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public UserDetailService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cliente = clienteRepository.findByEmail(email);
+        if (cliente == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+    }
+}
